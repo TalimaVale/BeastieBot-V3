dbf1c979bb5961690ac3f707357707b3ab629421
Turkka Mannila <turkka.mannila@vincit.fi>
2018-11-26 15:00:33 -0800

    Add bodyparser

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ require("dotenv").config();
 import config from "./config";
 const Koa = require("koa");
 const app = new Koa();
+const bodyParser = require("koa-bodyparser");
+
+app.use(bodyParser());
 
 app.use(async ctx => {
   ctx.body = "Hello World";
