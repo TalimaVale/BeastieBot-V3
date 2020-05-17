e370004bd89fecfd8a874a5ab0be122aed0e71f5
Sora <sora@sora.sora>
2020-05-17 09:30:30 +0100

    Added new config options for discord guild name, welcome channel and feed channel. Removed "values" entry for discord guild and channels Updated readme to give correct info for values of .etc options Updated .env.example default values

--- src/services/discord/ready/index.ts
+++ src/services/discord/ready/index.ts
@@ -1,11 +1,11 @@
-import { discordGuild } from "../../../utils/values";
+import config from "../../../config";
 
 export const handleDiscordReady = discordClient => {
   const guild = discordClient.guilds.cache.find(
-    g => g.name === discordGuild.guildName
+    g => g.name === config.DISCORD_GUILD_NAME
   );
   if (!guild) {
-    throw `Could not find guild specified ${discordGuild.guildName}`;
+    throw `Could not find guild specified ${config.DISCORD_GUILD_NAME}`;
   }
 
   // get guild
@@ -13,12 +13,12 @@ export const handleDiscordReady = discordClient => {
 
   // get welcome channel
   const discordWelcomeChId = guild.channels.cache.find(
-    ch => ch.name === discordGuild.welcomeCh
+    ch => ch.name === config.DISCORD_WELCOME_CHANNEL
   ).id;
 
   // get talimas-feed channel
   const discordTalimasFeedChId = guild.channels.cache.find(
-    ch => ch.name === discordGuild.talimasFeedCh
+    ch => ch.name === config.DISCORD_FEED_CHANNEL
   ).id;
 
   return { discordGuildId, discordWelcomeChId, discordTalimasFeedChId };
