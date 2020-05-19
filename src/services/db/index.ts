fc261c45d6afd4c6c2d25dc1a308b59f67182cb7
Daggersoath <9066690+Daggersoath@users.noreply.github.com>
2020-05-19 08:56:36 +0100

    Update src/services/db/index.ts
    Correcting AWS config to avoid having to use a @ts-ignore
    
    Co-authored-by: Scryptonite <scryptonite.mail@gmail.com>

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -4,7 +4,7 @@ import config from "../../config";
 AWS.config.update({
   region: "us-west-2",
   // @ts-ignore
-  endpoint: config.AWS_ENDPOINT
+  dynamodb: { endpoint: config.AWS_ENDPOINT }
 });
 
 const dynamoDB = new AWS.DynamoDB();
